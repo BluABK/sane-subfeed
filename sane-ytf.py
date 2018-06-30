40a13927ab267f79db4e6f2f8b139c077d8cb16f
BluABK <BluABK@users.noreply.github.com>
2018-06-30 17:17:08 +0200

    Integrated my_uploads.py sample into sane-ytf.py

---
+++ sane-ytf.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+# Retrieve the authenticated user's uploaded videos.
+# Sample usage:
+# python my_uploads.py
+
+import argparse
+import os
+import re
+
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+# Internal resources
+import my_uploads
+
+# The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
+# the OAuth 2.0 information for this application, including its client_id and
+# client_secret. You can acquire an OAuth 2.0 client ID and client secret from
+# the {{ Google Cloud Console }} at
+# {{ https://cloud.google.com/console }}.
+# Please ensure that you have enabled the YouTube Data API for your project.
+# For more information about using OAuth2 to access the YouTube Data API, see:
+#   https://developers.google.com/youtube/v3/guides/authentication
+# For more information about the client_secrets.json file format, see:
+#   https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
+CLIENT_SECRETS_FILE = 'client_secret.json'
+
+# This OAuth 2.0 access scope allows for read-only access to the authenticated
+# user's account, but not other types of account access.
+SCOPES = ['https://www.googleapis.com/auth/youtube.readonly']
+API_SERVICE_NAME = 'youtube'
+API_VERSION = 'v3'
+
+
+# Authorize the request and store authorization credentials.
+def get_authenticated_service():
+    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+    credentials = flow.run_console()
+    return build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
+
+
+def get_my_uploads(youtube):
+    try:
+        uploads_playlist_id = my_uploads.get_my_uploads_list(youtube)
+        if uploads_playlist_id:
+            my_uploads.list_my_uploaded_videos(youtube, uploads_playlist_id)
+        else:
+            print('There is no uploaded videos playlist for this user.')
+    except HttpError as e:
+        print('An HTTP error %d occurred:\n%s' % (e.resp.status, e.content))
+
+
+if __name__ == '__main__':
+    youtube = get_authenticated_service()
+    get_my_uploads(youtube)
