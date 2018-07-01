b863a2b00fdf316584540690a531520d5762f20f
BluABK <BluABK@users.noreply.github.com>
2018-07-01 03:45:48 +0200

    Made new video uploads sort reverse chronologically, moved some main code into functions and added ability to actually print the subscription feed.

--- sane-ytf.py
+++ sane-ytf.py
@@ -13,6 +13,7 @@ import google_auth_oauthlib.flow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from google_auth_oauthlib.flow import InstalledAppFlow
+from collections import OrderedDict
 
 # Internal resources
 # import my_uploads
@@ -99,46 +100,59 @@ def playlist_items_list_by_playlist_id(**kwargs):
     return response
 
 
-def list_uploaded_videos(uploads_playlist_id):
+def list_uploaded_videos(uploads_playlist_id, debug=False, max=25):
     # Retrieve the list of videos uploaded to the authenticated user's channel.
     playlistitems_list_request = youtube.playlistItems().list(
-        playlistId=uploads_playlist_id,
+        maxResults=5,
         part='snippet',
-        maxResults=5
+        playlistId=uploads_playlist_id
+
     )
 
     print('Videos in list %s' % uploads_playlist_id)
+    videos = []
     while playlistitems_list_request:
         playlistitems_list_response = playlistitems_list_request.execute()
 
-        # Print information about each video.
+        # Grab information about each video.
         for playlist_item in playlistitems_list_response['items']:
+            channel_title = playlist_item['snippet']['channelTitle']
             title = playlist_item['snippet']['title']
             video_id = playlist_item['snippet']['resourceId']['videoId']
             date_published = playlist_item['snippet']['publishedAt']
             description = playlist_item['snippet']['description']
             thumbnails = playlist_item['snippet']['thumbnails']
 
-            print('%s\t%s%s\t%s\t%s' % (date_published, YT_VIDEO_URL, video_id, title, repr(description)))
+            if debug:
+                print('%s\t%s%s\t%s:\t%s\t%s' % (date_published, YT_VIDEO_URL, video_id, channel_title, title,
+                                                repr(description)))
+
+            videos.append({'date': date_published, 'id': video_id, 'channel': channel_title, 'title': title,
+                           'description': description, 'thumbnails': thumbnails})
+            #videos.append([date_published, video_id, channel_title, title, description])
+            if len(videos) >= max:
+                return videos
 
         playlistitems_list_request = youtube.playlistItems().list_next(
             playlistitems_list_request, playlistitems_list_response)
+    return videos
 
 
-if __name__ == '__main__':
-    youtube = get_authenticated_service()
-
+def get_uploads(channel_id, debug=False, max=25):
     # Get channel
     test_chan = channels_list_by_id(part='snippet,contentDetails,statistics',
-                                    id='UCZn2JQsQd3MXH07aI9TDI3g')
+                                    id=channel_id)
 
     # Get ID of uploads playlist
     test_chan_uploads_playlist_id = test_chan['items'][0]['contentDetails']['relatedPlaylists']['uploads']
 
     # Get playlistListResponse item of uploads playlist
-    list_uploaded_videos(test_chan_uploads_playlist_id)
+    return list_uploaded_videos(test_chan_uploads_playlist_id, debug=debug, max=max)
 
-    exit()
+
+if __name__ == '__main__':
+    youtube = get_authenticated_service()
+
     response = subscriptions_list_my_subscriptions(client=youtube,
                                                    part='snippet,contentDetails',
                                                    mine=True)
@@ -148,12 +162,14 @@ if __name__ == '__main__':
     print("Found %s subscriptions." % subscription_total)
 
     if 'nextPageToken' in response:
+        #next_page = False # FIXME: DEBUG HACK: skip processing entire sublist
         next_page = True
         next_page_token = response['nextPageToken']
-        print("DEBUG: Querying PageToken: ", end='')
+        #print("DEBUG: Querying PageToken: ", end='')
+        print("Querying PageTokens...")
 
         while next_page:
-            print(", %s" % next_page_token, end='')
+            #print(", %s" % next_page_token, end='')
             this_response = subscriptions_list_my_subscriptions(client=youtube,
                                                                 part='snippet,contentDetails',
                                                                 mine=True,
@@ -167,11 +183,16 @@ if __name__ == '__main__':
                 next_page = False
 
     subscribed_channels = {}
+    ocd_longest_channel_title = 0
     for item in subscription_list:
         item_id = item['id']
         channel_title = item['snippet']['title']
         channel_description = item['snippet']['description']
         channel_id = item['snippet']['resourceId']['channelId']
+
+        # Fix spacing issues between Channel and Video Title
+        if len(channel_title) > ocd_longest_channel_title:
+            ocd_longest_channel_title = len(channel_title)
 
         subscribed_channels.update({channel_id: channel_title})
 
@@ -180,6 +201,25 @@ if __name__ == '__main__':
     if subscription_total != len(subscription_list):
         print("WARNING: Subscription list mismatched advertised length (%s/%s)!" % (len(subscription_list),
                                                                                     subscription_total))
+    new_videos_by_channel = {}
+    new_videos_by_timestamp = {}
+    for channel in subscription_list:
+        channel_title = channel['snippet']['title']
+        channel_id = channel['snippet']['resourceId']['channelId']
+        print("Fetching Uploaded videos for channel: %s" % channel_title)
 
-    print("================ DEBUG SPAM COMMENCES BELOW THIS LINE ================")
-    print(subscription_list)
+        new_videos_channel = get_uploads(channel_id, debug=True)
+
+        new_videos_by_channel.update({channel_title: new_videos_channel})
+        for video in new_videos_channel:
+            new_videos_by_timestamp.update({video['date']: video})
+
+    new_videos = OrderedDict(sorted(new_videos_by_timestamp.items(), reverse=True))
+
+    print("Supposedly sane and datestamp sorted subscription feed:")
+    for i, (date, video) in enumerate(new_videos.items()):
+        offset = ocd_longest_channel_title - len(video['channel'])
+        spacing = " " * offset + " " * 4
+        print('%s\t%s%s\t%s:%s%s\t%s' % (video['date'], YT_VIDEO_URL, video['id'], video['channel'], spacing,
+                                           video['title'], repr(video['description'])))
+
