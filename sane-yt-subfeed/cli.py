5a80b29b80627a6957eacfc03acf6641b804bbee
BluABK <BluABK@users.noreply.github.com>
2018-07-08 20:39:17 +0200

    Add sys.excepthook override, due to PyQt5 not raising exceptions >_<

--- sane-yt-subfeed/cli.py
+++ sane-yt-subfeed/cli.py
@@ -1,3 +1,5 @@
+import sys
+
 import click
 
 from main import run_with_gui, run_print
@@ -9,6 +11,22 @@ def cli(no_gui):
     if no_gui:
         run_print()
     else:
+        """
+        PyQT raises and catches exceptions, but doesn't pass them along. 
+        Instead it just exits with a status of 1 to show an exception was caught. 
+        """
+        # Back up the reference to the exceptionhook
+        sys._excepthook = sys.excepthook
+
+        def my_exception_hook(exctype, value, traceback):
+            # Print the error and traceback
+            print(exctype, value, traceback)
+            # Call the normal Exception hook after
+            sys._excepthook(exctype, value, traceback)
+            sys.exit(1)
+
+        # Set the exception hook to our wrapping function
+        sys.excepthook = my_exception_hook
         run_with_gui()
 
 
