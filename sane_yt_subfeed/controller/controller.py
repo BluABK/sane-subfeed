286202c0cb37d17e575ad796ed076ed94d68126a
unknown <nikert_ludvik@hotmail.com>
2018-07-13 01:21:54 +0200

    Fixed len() on list

--- sane_yt_subfeed/controller/controller.py
+++ sane_yt_subfeed/controller/controller.py
@@ -42,7 +42,7 @@ class Controller:
                     subscription_feed = refresh_and_get_newest_videos(self.vid_limit, filter_downloaded=filter_dl)
         else:
             subscription_feed = refresh_and_get_newest_videos(self.vid_limit, filter_downloaded=filter_dl)
-        model = MainModel([], subscription_feed, 30)
+        model = MainModel([], subscription_feed, 200)
 
         app = QApplication(sys.argv)
         window = MainWindow(model)
