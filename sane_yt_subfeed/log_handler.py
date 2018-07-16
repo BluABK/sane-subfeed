bed6c2f9a3207d12ea3ccf7ab074157409772e6d
BluABK <BluABK@users.noreply.github.com>
2018-07-16 21:58:55 +0200

    Added log_handler.py

---
+++ sane_yt_subfeed/log_handler.py
@@ -0,0 +1,44 @@
+import os
+import logging
+
+"""
+Logging levels
+CRITICAL 	50
+ERROR 	    40
+WARNING 	30
+INFO 	    20
+DEBUG 	    10
+NOTSET 	    0
+"""
+
+OS_PATH = os.path.dirname(__file__)
+LOGDIR = os.path.join(OS_PATH, 'logs')
+
+# create logger with 'spam_application'
+logger = logging.getLogger('debug')
+logger.setLevel(logging.DEBUG)
+# create file handler which logs even debug messages
+if not os.path.exists(LOGDIR):
+    os.makedirs(LOGDIR)
+fh = logging.FileHandler(os.path.join(LOGDIR, 'debug.log'))
+fh.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+# logger.info('creating an instance of auxiliary_module.Auxiliary')
+# a = auxiliary_module.Auxiliary()
+# logger.info('created an instance of auxiliary_module.Auxiliary')
+# logger.info('calling auxiliary_module.Auxiliary.do_something')
+# a.do_something()
+# logger.info('finished auxiliary_module.Auxiliary.do_something')
+# logger.info('calling auxiliary_module.some_function()')
+# auxiliary_module.some_function()
+# logger.info('done with auxiliary_module.some_function()')
