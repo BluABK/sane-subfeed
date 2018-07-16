c151d39559f606a86117795f868d4c8d015a7027
Acca <nikert.ludvik@gmail.com>
2018-07-16 23:06:57 +0200

    Added functionality for the model to refresh filtered videos when /2

--- sane_yt_subfeed/controller/view_models.py
+++ sane_yt_subfeed/controller/view_models.py
@@ -2,6 +2,7 @@ from PyQt5.QtCore import QThread
 
 # FIXME: imp*
 from sane_yt_subfeed.controller.listeners import *
+from sane_yt_subfeed.database.read_operations import get_newest_stored_videos
 
 
 class MainModel:
@@ -32,6 +33,14 @@ class MainModel:
 
     def hide_video_item(self, index):
         del self.filtered_videos[index]
-        if len(self.filtered_videos) < self.videos_limit:
-            # TODO: implement this with listener
-            print('Filtered videos less than videos limit: TODO')
+        if len(self.filtered_videos) < self.videos_limit/2:
+            self.db_update_videos()
+            # FIXME: only does filtered videos
+            logger.info('Reduced view models filtered_videos to /2, requesting new videos from db')
+
+    def db_update_videos(self, filtered=True):
+        if filtered:
+            self.filtered_videos = get_newest_stored_videos(self.videos_limit, filtered)
+            self.grid_view_listener.hiddenVideosChanged.emit()
+        else:
+            self.videos = get_newest_stored_videos(self.videos_limit, filtered)
