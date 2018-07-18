1cb174e1b0907a3926336eae86b65a7301ebb043
Acca <nikert.ludvik@gmail.com>
2018-07-18 18:19:56 +0200

    Made video_tiles to be fixed size

--- sane_yt_subfeed/gui/views/grid_view/channel_tile.py
+++ sane_yt_subfeed/gui/views/grid_view/channel_tile.py
@@ -7,19 +7,23 @@ class ChannelTile(QLabel):
     def __init__(self, text, parent):
         QLabel.__init__(self, text)
         self.parent = parent
+
+        margins = self.parent.layout.getContentsMargins()
+        self.setFixedSize(self.parent.width()-margins[0]-margins[2], (self.parent.height()-4*margins[3]) * 0.07)
+
         t_font = self.font()
         t_font.setStyleHint(QFont.Helvetica)
+        t_font.setPixelSize(self.height())
         self.setFont(t_font)
 
-    def resizeEvent(self, *args, **kwargs):
-        margins = self.parent.layout.getContentsMargins()
-        prev_h = (self.parent.height() - (4 * margins[1] + 4 * margins[3])) * 0.2 + (
-                    self.parent.height() - (4 * margins[1] + 4 * margins[3])) * 0.6
-        self.setGeometry(margins[0], prev_h + (3 * margins[1] + 2 * margins[3]), self.parent.width() - 2 * margins[2],
-                         (self.parent.height() - (4 * margins[1] + 4 * margins[3])) * 0.09)
-
-    def update_font(self):
-        t_font = self.font()
-        margins = self.parent.layout.getContentsMargins()
-        t_font.setPixelSize((self.height()-margins[1]-margins[3]))
-        self.setFont(t_font)
+    # def resizeEvent(self, *args, **kwargs):
+    #     margins = self.parent.layout.getContentsMargins()
+    #     prev_h = (self.parent.height() - (4 * margins[1] + 4 * margins[3])) * 0.2 + (
+    #                 self.parent.height() - (4 * margins[1] + 4 * margins[3])) * 0.6
+    #     self.setGeometry(margins[0], prev_h + (3 * margins[1] + 2 * margins[3]), self.parent.width() - 2 * margins[2],
+    #                      (self.parent.height() - (4 * margins[1] + 4 * margins[3])) * 0.09)
+    #
+    # def update_font(self):
+    #     t_font = self.font()
+    #     t_font.setPixelSize(self.height())
+    #     self.setFont(t_font)
--- sane_yt_subfeed/gui/views/grid_view/title_tile.py
+++ sane_yt_subfeed/gui/views/grid_view/title_tile.py
@@ -9,24 +9,29 @@ class TitleTile(QLabel):
     def __init__(self, text, parent):
         QLabel.__init__(self, text)
         self.parent = parent
+
+        margins = self.parent.layout.getContentsMargins()
+        self.setFixedSize(self.parent.width()-margins[0]-margins[2], (self.parent.height()-4*margins[3]) * 0.18)
+
         self.setWordWrap(True)
         self.setAlignment(QtCore.Qt.AlignLeft | QtCore.Qt.AlignTop)
+
+        # t_font = QFont()
         t_font = self.font()
         t_font.setWeight(QFont.DemiBold)
         t_font.setStyleHint(QFont.Helvetica)
+        # t_font.setPixelSize(5)
+        t_font.setPixelSize(self.height()*0.45)
         self.setFont(t_font)
 
     def update_font(self):
-        t_font = self.font()
-        margins = self.parent.layout.getContentsMargins()
-        t_font.setPixelSize((self.height()-margins[1]-margins[3])*0.45)
-        self.setFont(t_font)
-        metrics = QFontMetrics(t_font)
+
+        metrics = QFontMetrics(self.font())
         elided = metrics.elidedText(self.parent.video.title, Qt.ElideRight, self.width() * 1.9)
         self.setText(elided)
 
-    def resizeEvent(self, *args, **kwargs):
-        margins = self.parent.layout.getContentsMargins()
-        prev_h = (self.parent.height() - (4 * margins[1] + 4 * margins[3])) * 0.6
-        self.setGeometry(margins[0], prev_h + (2 * margins[1] + margins[3]), self.parent.width() - 2 * margins[2],
-                         (self.parent.height() - (4 * margins[1] + 4 * margins[3])) * 0.2)
+    # def resizeEvent(self, *args, **kwargs):
+    #     margins = self.parent.layout.getContentsMargins()
+    #     prev_h = (self.parent.height() - (4 * margins[1] + 4 * margins[3])) * 0.6
+    #     self.setGeometry(margins[0], prev_h + (2 * margins[1] + margins[3]), self.parent.width() - 2 * margins[2],
+    #                      (self.parent.height() - (4 * margins[1] + 4 * margins[3])) * 0.2)
