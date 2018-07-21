d094ed0606466d1ae6b21b7fbf4cce3a7a4bc519
unknown <nikert_ludvik@hotmail.com>
2018-07-21 17:26:13 +0200

    ProgressBar works for update_videos

--- sane_yt_subfeed/youtube/update_videos.py
+++ sane_yt_subfeed/youtube/update_videos.py
@@ -1,6 +1,7 @@
 import time
 
 from sane_yt_subfeed.config_handler import read_config
+from sane_yt_subfeed.controller.listeners import ProgressBar
 from sane_yt_subfeed.database.orm import db_session
 from sane_yt_subfeed.database.write_operations import UpdateVideosThread
 from sane_yt_subfeed.database.video import Video
@@ -17,13 +18,16 @@ YOUTUBE_PARM_PLIST = "playlist?list ="
 YT_VIDEO_URL = YOUTUBE_URL + YOUTUBE_PARM_VIDEO
 
 
-def refresh_uploads():
+def refresh_uploads(progress_bar_listener: ProgressBar=None):
     thread_increment = 0
     thread_list = []
     videos = []
     cached_subs = read_config('Debug', 'cached_subs')
     subscriptions = get_subscriptions(cached_subs)
     youtube_keys = load_keys(subscriptions)
+    if progress_bar_listener:
+        progress_bar_listener.resetBar.emit()
+        progress_bar_listener.setMaximum.emit(2*len(subscriptions))
 
     channels_limit = read_config('Debug', 'channels_limit')
     for channel, youtube in tqdm(zip(subscriptions, youtube_keys), desc="Creating video update threads",
@@ -31,14 +35,27 @@ def refresh_uploads():
         thread = GetUploadsThread(thread_increment, youtube, channel.id, channel.playlist_id, videos, 1)
         thread_list.append(thread)
         thread_increment += 1
+
+
         if 0 < channels_limit <= thread_increment:
             break
         # if thread_increment >= thread_limit:
         #     break
+
+    if progress_bar_listener:
+        progress_bar_listener.setText.emit('Starting video update threads')
+
     for t in tqdm(thread_list, desc="Starting video update threads", disable=read_config('Debug', 'disable_tqdm')):
+        if progress_bar_listener:
+            progress_bar_listener.updateProgress.emit()
         t.start()
 
+    if progress_bar_listener:
+        progress_bar_listener.setText.emit('Waiting for video update threads')
+
     for t in tqdm(thread_list, desc="Waiting for video update threads", disable=read_config('Debug', 'disable_tqdm')):
+        if progress_bar_listener:
+            progress_bar_listener.updateProgress.emit()
         t.join()
 
     return sorted(videos, key=lambda video: video.date_published, reverse=True)
@@ -65,7 +82,8 @@ def generate_keys(key_number):
     keys = []
     threads = []
 
-    for _ in tqdm(range(key_number), desc="Starting key generation threads", disable=read_config('Debug', 'disable_tqdm')):
+    for _ in tqdm(range(key_number), desc="Starting key generation threads",
+                  disable=read_config('Debug', 'disable_tqdm')):
         t = GenerateKeys(keys)
         t.start()
         threads.append(t)
