0177949804ca9cb226b407252476f17b3c62b9e2
unknown <nikert_ludvik@hotmail.com>
2018-08-03 19:48:27 +0200

    The system now uses config for media player

--- sane_yt_subfeed/gui/views/play_view/play_tile.py
+++ sane_yt_subfeed/gui/views/play_view/play_tile.py
@@ -1,8 +1,9 @@
 import subprocess
 
 from PyQt5.QtCore import Qt, QEvent
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QMenu
 
+from sane_yt_subfeed.config_handler import read_config
 from sane_yt_subfeed.gui.views.grid_view.video_tile import VideoTile
 from sane_yt_subfeed.log_handler import logger
 
@@ -24,10 +25,36 @@ class PlayTile(VideoTile):
         if QMouseEvent.button() == Qt.LeftButton and QApplication.keyboardModifiers() == Qt.ControlModifier:
             logger.error("Not Implemented: Select video")
         elif QMouseEvent.button() == Qt.LeftButton:
-            self.play_vid(self.video.vid_path)
-            self.mark_watched()
+            self.play_vid(self.video.vid_path, self.get_default_player())
+
+    def get_default_player(self):
+        config_default_player = read_config('Player', 'default_player')
+        if config_default_player == "":
+            return None
+        else:
+            return config_default_player
 
 
-    def play_vid(self, file_path, player="mpc-hc64"):
-        subprocess.Popen([player, file_path])
+    def play_vid(self, file_path, player):
+        self.mark_watched()
+        if player:
+            subprocess.Popen([player, file_path])
+        else:
+            subprocess.Popen([file_path])
 
+
+
+    def contextMenuEvent(self, event):
+        """
+        Override context menu event to set own custom menu
+        :param event:
+        :return:
+        """
+        menu = QMenu(self)
+        copy_url_action = menu.addAction("Copy link")
+        discard_item_action = menu.addAction("Discard video")
+        action = menu.exec_(self.mapToGlobal(event.pos()))
+        if action == copy_url_action:
+            self.copy_url()
+        elif action == discard_item_action:
+            self.mark_discarded()
