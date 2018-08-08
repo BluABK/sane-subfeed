efc14843aad5980d77014a9a248eaed3c50f6fb2
Acca <nikert.ludvik@gmail.com>
2018-08-08 16:15:02 +0200

    Q_labels are now set based on video for update_videos

--- sane_yt_subfeed/gui/views/grid_view/play_view/play_view.py
+++ sane_yt_subfeed/gui/views/grid_view/play_view/play_view.py
@@ -27,3 +27,4 @@ class PlayView(GridView):
     def update_videos(self):
         for q_label, video in zip(self.q_labels.values(), self.main_model.downloaded_videos):
             q_label.set_video(video)
+            self.q_labels[video.video_id] = q_label
