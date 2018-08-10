692c180f33a07832ac1c69a8b666521fbc20ed84
Acca <nikert.ludvik@gmail.com>
2018-08-11 01:53:47 +0200

    Added GenericLineEdit

---
+++ sane_yt_subfeed/gui/views/config_view/config_items/line_edit.py
@@ -0,0 +1,19 @@
+from PyQt5.QtWidgets import QLineEdit
+
+from sane_yt_subfeed.config_handler import read_config, set_config
+
+
+class GenericLineEdit(QLineEdit):
+    def __init__(self, parent, description, cfg_section, cfg_option):
+        super(QLineEdit, self).__init__(parent=parent)
+        self.description = description
+        self.cfg_section = cfg_section
+        self.cfg_option = cfg_option
+        self.config_value = read_config(self.cfg_section, self.cfg_option, literal_eval=False)
+
+        self.setPlaceholderText(self.config_value)
+
+        self.returnPressed.connect(self.save_option)
+
+    def save_option(self):
+        set_config(self.cfg_section, self.cfg_option, self.text())
