9ded8f65799d7f9de8e37f7f4d0411f920e47929
Acca <nikert.ludvik@gmail.com>
2018-08-11 02:20:39 +0200

    Added validators

--- sane_yt_subfeed/gui/views/config_view/config_view.py
+++ sane_yt_subfeed/gui/views/config_view/config_view.py
@@ -1,9 +1,11 @@
 # PyQt5
+from PyQt5.QtGui import QIntValidator
 from PyQt5.QtWidgets import QWidget, QGridLayout, QLabel, QCheckBox, QComboBox
 
 # Internal
 from sane_yt_subfeed.config_handler import read_config, DEFAULTS, get_size, get_options
 # import sane_yt_subfeed.gui.views.config_view.checkbox as checkbox
+from sane_yt_subfeed.gui.views.config_view.config_item_types import THUMBNAIL_QUALITIES, TT_FONT_SIZES
 from sane_yt_subfeed.gui.views.config_view.config_items import checkbox, combobox
 from sane_yt_subfeed.gui.views.config_view.config_items.checkbox import GenericConfigCheckBox
 from sane_yt_subfeed.gui.views.config_view.config_items.combobox import GenericConfigComboBox
@@ -74,7 +76,7 @@ class ConfigViewWidget(QWidget):
 
         return value  # Needed for connected listeners etc
 
-    def add_option_line_edit(self, description, cfg_section, cfg_option):
+    def add_option_line_edit(self, description, cfg_section, cfg_option, cfg_validator=None):
         """
         Add an option w/ text value to the ConfigView layout and increment the grid offset.
         :param cfg_option:
@@ -83,7 +85,7 @@ class ConfigViewWidget(QWidget):
         :return:
         """
         option = QLabel(description)
-        value = GenericLineEdit(self, description, cfg_section, cfg_option)
+        value = GenericLineEdit(self, description, cfg_section, cfg_option, cfg_validator=cfg_validator)
         self.layout.addWidget(option, self.offset, 0)
         self.layout.addWidget(value, self.offset, 1)
         self.offset += 1
@@ -109,23 +111,12 @@ class ConfigViewWidget(QWidget):
     def add_option_combobox(self, description, cfg_section, cfg_option, items):
         """
         Add an option w/ value to the ConfigView layout and increment the grid offset.
-        :param numeric:
         :param items:
         :param cfg_option:
         :param cfg_section:
         :param description:
         :return:
         """
-        # if numeric:
-        #     items = [str(i) for i in items]
-        #     if items[0] == 'Disabled':
-        #         value_offset = -1
-        #     elif int(items[0]) == 0:
-        #         value_offset = 0
-        #     elif int(items[0]) == 1:
-        #         value_offset = 1
-        #     else:
-        #         value_offset = 1
 
         formated_items = [format(item) for item in items]
 
@@ -142,25 +133,23 @@ class ConfigViewWidget(QWidget):
         Populate the layout with sections, options and (editable) values
         :return:
         """
-        combox_w_disabled = list(range(-1, 1000))
-        combox_w_disabled[0] = 'Disabled'
-        thumb_qualities = ['maxres', 'standard', 'high', 'medium', 'default']
-        tt_font_sizes = ['h1', 'h2', 'h3', 'h4', 'h5', 'p']
 
         # Section [Gui]
         self.add_section('{}GUI{}'.format(self.deco_l, self.deco_r))
         self.add_option_checkbox('Launch GUI', 'Gui', 'launch_gui')
         self.add_option_checkbox('Hide downloaded videos from feed', 'Gui',
                                  'hide_downloaded')
-        self.add_option_combobox('Grid view X', 'Gui', 'grid_view_x', list(range(1, 100)))
-        self.add_option_combobox('Grid view Y', 'Gui', 'grid_view_y', list(range(1, 100)))
+        self.add_option_line_edit('Grid view X', 'Gui', 'grid_view_x', cfg_validator=QIntValidator())
+        self.add_option_line_edit('Grid view Y', 'Gui', 'grid_view_y', cfg_validator=QIntValidator())
         self.add_option_checkbox('Grey background on old (1d+) videos', 'Gui', 'grey_old_videos')
-        self.add_option_combobox('\tGrid tile height (px)', 'Gui', 'tile_pref_height', list(range(1, 1000)))
-        self.add_option_combobox('\tGrid tile width (px)', 'Gui', 'tile_pref_width', list(range(1, 1000)))
+        self.add_option_line_edit('\tGrid tile height (px)', 'Gui', 'tile_pref_height', cfg_validator=QIntValidator())
+        self.add_option_line_edit('\tGrid tile width (px)', 'Gui', 'tile_pref_width', cfg_validator=QIntValidator())
         self.add_option_checkbox('Embed thumbnails in tooltips', 'Gui', 'tooltip_pictures')
-        self.add_option_combobox('\tTooltip picture width', 'Gui', 'tooltip_picture_width', list(range(1, 1000)))
-        self.add_option_combobox('\tTooltip picture height', 'Gui', 'tooltip_picture_height', list(range(1, 1000)))
-        self.add_option_combobox('\tTooltip picture font size', 'Gui', 'tooltip_picture_size', list(range(1, 1000)))
+        self.add_option_line_edit('\tTooltip picture width', 'Gui', 'tooltip_picture_width',
+                                  cfg_validator=QIntValidator())
+        self.add_option_line_edit('\tTooltip picture height', 'Gui', 'tooltip_picture_height',
+                                  cfg_validator=QIntValidator())
+        self.add_option_combobox('\tTooltip picture font size', 'Gui', 'tooltip_picture_size', TT_FONT_SIZES)
         self.add_option_checkbox('Keep Aspect Ratop on resized thumbnails', 'Gui', 'keep_thumb_ar')
         self.add_option_checkbox('Auto copy to clipboard', 'Gui', 'enable_auto_copy_to_clipboard')
 
@@ -169,7 +158,7 @@ class ConfigViewWidget(QWidget):
         self.add_option_checkbox('Debug prints', 'Debug', 'debug')
         self.add_option_checkbox('Cache subscriptions', 'Debug', 'cached_subs')
         self.add_option_checkbox('Start with cached videos', 'Debug', 'start_with_stored_videos')
-        self.add_option_combobox('Channel limit', 'Debug', 'channels_limit', list(range(-1, 100)))
+        self.add_option_line_edit('Channel limit', 'Debug', 'channels_limit', cfg_validator=QIntValidator())
         self.add_option_checkbox('Use playlistItems', 'Debug', 'use_playlistitems')
         self.add_option_checkbox('Disable tooltips', 'Debug', 'disable_tooltips')
         self.add_option_checkbox('Disable tqdm (cli)', 'Debug', 'disable_tqdm')
@@ -181,39 +170,41 @@ class ConfigViewWidget(QWidget):
 
         # Section [Model]
         self.add_section('{}Model{}'.format(self.deco_l, self.deco_r))
-        self.add_option_combobox('Videos to load by default', 'Model', 'loaded_videos', list(range(1, 1000)))
+        self.add_option_line_edit('Videos to load by default', 'Model', 'loaded_videos', cfg_validator=QIntValidator())
 
         # Section [Requests]
         self.add_section('{}Requests{}'.format(self.deco_l, self.deco_r))
         self.add_option_checkbox('Use tests', 'Requests', 'use_tests')
-        self.add_option_combobox('Missed video limit', 'Requests', 'miss_limit', list(range(0, 201)))
-        self.add_option_combobox('Test pages', 'Requests', 'test_pages', list(range(0, 201)))
-        self.add_option_combobox('Additional list pages', 'Requests', 'extra_list_pages', list(range(0, 201)))
-        self.add_option_combobox('Deep search API quota limit per request (in K)', 'Requests', 'deep_search_quota_k',
-                                 list(range(1, 1000)))
-        self.add_option_combobox('Filter videos older than (days)', 'Requests', 'filter_videos_days_old',
-                                 list(range(-1, 100)))
+        self.add_option_line_edit('Missed video limit', 'Requests', 'miss_limit', cfg_validator=QIntValidator())
+        self.add_option_line_edit('Test pages', 'Requests', 'test_pages', cfg_validator=QIntValidator())
+        self.add_option_line_edit('Additional list pages', 'Requests', 'extra_list_pages',
+                                  cfg_validator=QIntValidator())
+        self.add_option_line_edit('Deep search API quota limit per request (in K)', 'Requests', 'deep_search_quota_k',
+                                  cfg_validator=QIntValidator())
+        self.add_option_line_edit('Filter videos older than (days)', 'Requests', 'filter_videos_days_old',
+                                  cfg_validator=QIntValidator())
 
         # Section [Thumbnails]
         self.add_section('{}Thumbnails{}'.format(self.deco_l, self.deco_r))
         self.add_option_checkbox('Force download best quality, based on prioritised list',
                                  'Thumbnails', 'force_download_best')
-        self.add_option_combobox('1. Priority', 'Thumbnails', '0',  thumb_qualities)
-        self.add_option_combobox('2. Priority', 'Thumbnails', '1',  thumb_qualities)
-        self.add_option_combobox('3. Priority', 'Thumbnails', '2',  thumb_qualities)
-        self.add_option_combobox('4. Priority', 'Thumbnails', '3',  thumb_qualities)
-        self.add_option_combobox('5. Priority', 'Thumbnails', '4',  thumb_qualities)
+        self.add_option_combobox('1. Priority', 'Thumbnails', '0', THUMBNAIL_QUALITIES)
+        self.add_option_combobox('2. Priority', 'Thumbnails', '1', THUMBNAIL_QUALITIES)
+        self.add_option_combobox('3. Priority', 'Thumbnails', '2', THUMBNAIL_QUALITIES)
+        self.add_option_combobox('4. Priority', 'Thumbnails', '3', THUMBNAIL_QUALITIES)
+        self.add_option_combobox('5. Priority', 'Thumbnails', '4', THUMBNAIL_QUALITIES)
 
         # Section [Threading]
         self.add_section('{}Threading{}'.format(self.deco_l, self.deco_r))
-        self.add_option_combobox('Image/thumbnail download thread limit', 'Threading', 'img_threads', list(range(1, 1001)))
+        self.add_option_line_edit('Image/thumbnail download thread limit', 'Threading', 'img_threads',
+                                  cfg_validator=QIntValidator())
 
         # Section [Play]
         self.add_section('{}View: Playback{}'.format(self.deco_l, self.deco_r))
         self.add_option_line_edit('YouTube video directory', 'Play', 'yt_file_path')
         self.add_option_checkbox('Disable directory listener (inotify)', 'Play', 'disable_dir_listener')
         self.add_option_checkbox('Use URL as path', 'Play', 'use_url_as_path')
-        self.add_option_combobox('Default watch priority', 'Play', 'default_watch_prio', list(range(0, 101)))
+        self.add_option_line_edit('Default watch priority', 'Play', 'default_watch_prio', cfg_validator=QIntValidator())
 
         # Section [Youtube-dl]
         self.add_section('{}Downloading / youtube-dl{}'.format(self.deco_l, self.deco_r))
@@ -240,8 +231,8 @@ class ConfigViewWidget(QWidget):
         # Section [Logging]
         self.add_section('{}Logging{}'.format(self.deco_l, self.deco_r))
         self.add_option_checkbox('Use socket instead of file', 'Logging', 'use_socket_log')
-        self.add_option_combobox('Log level', 'Logging', 'log_level',  list(range(0, 11)))
-        self.add_option_combobox('Port', 'Logging', 'logging_port',  list(range(0, 65537)))
+        self.add_option_line_edit('Log level', 'Logging', 'log_level', cfg_validator=QIntValidator())
+        self.add_option_line_edit('Port', 'Logging', 'logging_port', cfg_validator=QIntValidator())
 
         # Section [Toolbar]
         self.add_section('{}Toolbar{}'.format(self.deco_l, self.deco_r))
