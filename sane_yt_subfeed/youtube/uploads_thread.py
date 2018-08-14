809f0ebc8252136f29f884ad6795a293a4a735b0
Acca <nikert.ludvik@gmail.com>
2018-08-14 14:18:49 +0200

    Made alle search() videos not in list() lookup extra information

--- sane_yt_subfeed/youtube/uploads_thread.py
+++ sane_yt_subfeed/youtube/uploads_thread.py
@@ -6,7 +6,7 @@ from sane_yt_subfeed.database.detached_models.video_d import GRAB_METHOD_SEARCH
 from sane_yt_subfeed.database.models import Channel
 from sane_yt_subfeed.database.orm import db_session
 from sane_yt_subfeed.youtube.youtube_requests import list_uploaded_videos_search, get_channel_uploads, \
-    list_uploaded_videos
+    list_uploaded_videos, list_uploaded_videos_videos
 from sane_yt_subfeed.log_handler import create_logger
 
 
@@ -60,19 +60,19 @@ class GetUploadsThread(threading.Thread):
             list_pages = 0
             list_videos = []
             search_videos = []
-            for test in channel.tests:
-                if test.test_pages > list_pages:
-                    list_pages = test.test_pages
-                if test.test_miss < miss or test.test_pages > pages:
-                    db_session.remove()
-                    list_uploaded_videos_search(self.youtube, self.channel_id, search_videos, self.search_pages)
-                    break
+            # for test in channel.tests:
+            #     if test.test_pages > list_pages:
+            #         list_pages = test.test_pages
+            #     if test.test_miss < miss or test.test_pages > pages:
+            #         db_session.remove()
+            list_uploaded_videos_search(self.youtube, self.channel_id, search_videos, self.search_pages)
+                    # break
             db_session.remove()
-            list_uploaded_videos(self.youtube, list_videos, self.playlist_id,
-                                 min(pages + extra_pages, list_pages + extra_pages))
+            # list_uploaded_videos(self.youtube, list_videos, self.playlist_id,
+            #                      min(pages + extra_pages, list_pages + extra_pages))
 
             if len(search_videos) > 0:
-                return_videos = self.merge_two_videos_list(list_videos, search_videos)
+                return_videos = self.merge_two_videos_list_grab_info(list_videos, search_videos)
             else:
                 return_videos = list_videos
             self.videos.extend(return_videos)
@@ -86,16 +86,8 @@ class GetUploadsThread(threading.Thread):
 
         self.job_done = True
 
-    #     videos_dict = {}
-    #     for video in videos:
-    #         if video.video_id in videos_dict:
-    #             if GRAB_METHOD_SEARCH in video.grab_methods:
-    #                 videos_dict[video.video_id].extend(video.grab_methods)
-    #             else:
-    #                 temp_vid =
-    #         else:
-    #             videos_dict[video.video_id] = video
-    def merge_two_videos_list(self, high_prio_list, low_prio_list):
+    @staticmethod
+    def merge_two_videos_list(high_prio_list, low_prio_list):
         high_prio_dict = {video.video_id: video for video in high_prio_list}
         low_prio_dict = {video.video_id: video for video in low_prio_list}
         output_list = []
@@ -107,6 +99,25 @@ class GetUploadsThread(threading.Thread):
             if video.video_id in low_prio_dict:
                 continue
             output_list.append(video)
+        return output_list
+
+    def merge_two_videos_list_grab_info(self, high_prio_list, low_prio_list):
+        high_prio_dict = {video.video_id: video for video in high_prio_list}
+        low_prio_dict = {video.video_id: video for video in low_prio_list}
+        output_list = []
+        low_prio_unique_ids = []
+        for video in high_prio_dict.values():
+            if video.video_id in low_prio_dict:
+                video.grab_methods.extend(low_prio_dict[video.video_id].grab_methods)
+            output_list.append(video)
+        for video in low_prio_dict.values():
+            if video.video_id in high_prio_dict:
+                continue
+            else:
+                low_prio_unique_ids.append(video.video_id)
+        if len(low_prio_unique_ids) > 0:
+            self.logger.info("Requesting additional information for search items: {}".format(low_prio_unique_ids))
+            output_list.extend(list_uploaded_videos_videos(self.youtube, low_prio_unique_ids, 50))
         return output_list
 
 
