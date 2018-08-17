bfcd30d45e3b47202684453bcd09e8a4ee18acd4
Acca <nikert.ludvik@gmail.com>
2018-08-17 13:49:52 +0200

    Added a static download view

---
+++ sane_yt_subfeed/gui/views/download_view/download_view.py
@@ -0,0 +1,20 @@
+import time
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QVBoxLayout
+
+from sane_yt_subfeed.gui.views.download_view.download_tile import DownloadTile
+
+
+class DownloadView(QWidget):
+
+    def __init__(self, parent, *args, **kwargs):
+        super(DownloadView, self).__init__(parent, *args, **kwargs)
+        self.sane_paret = parent
+
+        self.sane_layout = QVBoxLayout()
+        self.sane_layout.setAlignment(Qt.AlignTop)
+        self.setLayout(self.sane_layout)
+
+        self.test_widget = DownloadTile(self, "")
+        self.sane_layout.addWidget(self.test_widget)
