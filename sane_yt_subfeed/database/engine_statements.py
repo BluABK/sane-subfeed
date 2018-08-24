ed245ca89ecfb894c601c9c47574a7fbc80a0dc4
Acca <nikert.ludvik@gmail.com>
2018-08-24 17:02:16 +0200

    Made delete_sub_not_in_list sett subscribed = False, instead of deleting

--- sane_yt_subfeed/database/engine_statements.py
+++ sane_yt_subfeed/database/engine_statements.py
@@ -6,7 +6,7 @@ from sane_yt_subfeed.database.video import Video
 
 def update_video_statement_full(db_video):
     # video_ids = set(video.video_id for video in db_video)
-    return Video.__table__.update().where(Video.video_id==format(db_video.video_id)).values(
+    return Video.__table__.update().where(Video.video_id == format(db_video.video_id)).values(
         video_id=db_video.video_id,
         channel_title=db_video.channel_title,
         title=db_video.title,
@@ -25,16 +25,17 @@ def update_video_statement_full(db_video):
         watch_prio=db_video.watch_prio,
         date_downloaded=db_video.date_downloaded)
 
+
 def update_video_statement_selective(db_video, values):
     # video_ids = set(video.video_id for video in db_video)
-    return Video.__table__.update().where(Video.video_id==format(db_video.video_id)).values(*values)
-
-
+    return Video.__table__.update().where(Video.video_id == format(db_video.video_id)).values(*values)
 
 
 def update_thumbnails_path_stmt():
     return Video.__table__.update().where(Video.video_id == bindparam('_video_id')).values({
-            'thumbnail_path': bindparam('thumbnail_path')})
+        'thumbnail_path': bindparam('thumbnail_path')})
+
+
 #         'playlist_pos': bindparam('playlist_pos'),
 #         'url_video': bindparam('url_video'),
 #         'url_playlist_video': bindparam('url_playlist_video'),
@@ -63,7 +64,8 @@ def update_video_stmt():
 
 def update_channel_from_remote(channel):
     return Channel.__table__.update().where("id = '{}'".format(channel.id)).values(
-        title=channel.title, description=channel.description, snippet=channel.snippet, playlist_id=channel.playlist_id)
+        title=channel.title, description=channel.description, snippet=channel.snippet, playlist_id=channel.playlist_id,
+        subscribed=channel.subscribed, subscribed_override=channel.subscribed_override)
 
 
 def get_channel_by_id_stmt(channel):
