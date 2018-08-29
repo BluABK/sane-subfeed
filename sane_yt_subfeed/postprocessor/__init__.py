91f991fe1978e4ef118c9492650e8f58439fa92c
BluABK <BluABK@users.noreply.github.com>
2018-08-29 16:06:33 +0200

    Added youtube_dl's ffmpeg.py in order to change some hardcoded stuff

---
+++ sane_yt_subfeed/postprocessor/__init__.py
@@ -0,0 +1,40 @@
+from __future__ import unicode_literals
+
+from youtube_dl.postprocessor.embedthumbnail import EmbedThumbnailPP
+from .ffmpeg import (
+    FFmpegPostProcessor,
+    FFmpegEmbedSubtitlePP,
+    FFmpegExtractAudioPP,
+    FFmpegFixupStretchedPP,
+    FFmpegFixupM3u8PP,
+    FFmpegFixupM4aPP,
+    FFmpegMergerPP,
+    FFmpegMetadataPP,
+    FFmpegVideoConvertorPP,
+    FFmpegSubtitlesConvertorPP,
+)
+from youtube_dl.postprocessor.xattrpp import XAttrMetadataPP
+from youtube_dl.postprocessor.execafterdownload import ExecAfterDownloadPP
+from youtube_dl.postprocessor.metadatafromtitle import MetadataFromTitlePP
+
+
+def get_postprocessor(key):
+    return globals()[key + 'PP']
+
+
+__all__ = [
+    'EmbedThumbnailPP',
+    'ExecAfterDownloadPP',
+    'FFmpegEmbedSubtitlePP',
+    'FFmpegExtractAudioPP',
+    'FFmpegFixupM3u8PP',
+    'FFmpegFixupM4aPP',
+    'FFmpegFixupStretchedPP',
+    'FFmpegMergerPP',
+    'FFmpegMetadataPP',
+    'FFmpegPostProcessor',
+    'FFmpegSubtitlesConvertorPP',
+    'FFmpegVideoConvertorPP',
+    'MetadataFromTitlePP',
+    'XAttrMetadataPP',
+]
