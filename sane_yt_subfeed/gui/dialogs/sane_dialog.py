21950ad9c4f40a2e440d520c53a6459fc9b1c4d8
BluABK <BluABK@users.noreply.github.com>
2018-09-01 18:41:40 +0200

    SaneDialog(QDialog) now works, and will display filler attrib if called without overrides
    0 complaints from Qt on the parameters as well, which is rare \o/
    
    One FIXME about sizing window to text string width still remains.

--- sane_yt_subfeed/gui/dialogs/sane_dialog.py
+++ sane_yt_subfeed/gui/dialogs/sane_dialog.py
@@ -1,38 +1,35 @@
-from PyQt5.QtWidgets import QDialog, QLineEdit, QVBoxLayout, QPushButton, QHBoxLayout, QLabel
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QLabel
 
 from sane_yt_subfeed import create_logger
 
-OK = 'Ok'
-CANCEL = 'Cancel'
+
+TITLE = 'Unnamed SaneDialog'
+TEXT = 'This is a SaneDialog'
+DEFAULT_ALIGNMENT = Qt.AlignCenter
 
 
 class SaneDialog(QDialog):
-    def __init__(self, parent, text, ok_text=OK, cancel_text=CANCEL):
-        super().__init__(parent)
+    def __init__(self, parent, title=TITLE, text=TEXT, alignment=DEFAULT_ALIGNMENT, flags=Qt.WindowFlags):
+        super().__init__(parent, flags())
         self.logger = create_logger(__name__)
-        self.text = text
-        self.ok_text = ok_text
-        self.cancel_text = cancel_text
-        self.layout = QVBoxLayout()
-        self.button_layout = QHBoxLayout()
-        self.input_box = QLineEdit()
-        self.cancel_button = QPushButton(self)
-        self.ok_button = QPushButton(self)
+        self.title = title
+        self.text = QLabel(text, self)
+        self.alignment = alignment
         self.init_ui()
 
     def init_ui(self):
+        self.setWindowTitle(self.title)
+
+        # Define layout
+        layout = QVBoxLayout()
+
+        # Add content to layout
+        layout.addWidget(self.text, 0, self.alignment)
+
+        # Apply the layout
+        self.setLayout(layout)
+
         # FIXME: Resize width to len of info text string
         self.resize(360, 120)
-        self.logger.warn("Unimplemented SaneDialog")
-        self.layout.addWidget(QLabel(self.text))
-        self.layout.addWidget(self.input_box)
-        self.layout.addLayout(self.button_layout)
-        self.button_layout.addWidget(self.ok_button)
-        self.button_layout.addWidget(self.cancel_button)
-
-        self.input_box.setInputMask('Enter text here')
-        self.cancel_button.setText(self.cancel_text)
-        self.cancel_button.clicked.connect(self.reject)
-        self.ok_button.setText(self.ok_text)
-        # self.ok_button.clicked.connect()
 
