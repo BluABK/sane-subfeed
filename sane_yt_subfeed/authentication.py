169da9510a2c39053e751511a55614163d2dc2ff
BluABK <BluABK@users.noreply.github.com>
2018-12-12 14:37:40 +0100

    Fixed startup failures caused by missing {clients_secret,keys}.json
    Solution: Copy over public if no (custom) keys/secret has been supplied.

--- sane_yt_subfeed/authentication.py
+++ sane_yt_subfeed/authentication.py
@@ -1,5 +1,6 @@
 import os
 import json
+import shutil  # For file copying
 
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
@@ -19,7 +20,19 @@ API_SERVICE_NAME = 'youtube'
 API_VERSION = 'v3'
 
 
+def handle_missing_keys():
+    """
+    Checks if a keys.json exists in the expected location, if it does not then copy the public version.
+    :return:
+    """
+    if not os.path.isfile(KEYS_FILE):
+        logger.warning("keys.json file was not found: Installing public version.")
+        shutil.copyfile(os.path.join(OS_PATH, "resources", "keys_public.json"),
+                        KEYS_FILE)
+
+
 def get_api_key():
+    handle_missing_keys()
     with open(KEYS_FILE) as keys_json_data_file:
         keys = json.load(keys_json_data_file)
     return keys['api_key']
@@ -31,7 +44,19 @@ def youtube_auth_oauth():
     :return:
     """
     logger.info("OAuth: Authorising API...")
-    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+    try:
+        flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+    except FileNotFoundError as exc_fnfe:
+        logger.warning("client_secret.json file was not found: Installing public version.")
+        if "client_secret.json" in str(exc_fnfe):
+            shutil.copyfile(os.path.join(OS_PATH, "resources", "client_secret_public.json"),
+                            os.path.join(OS_PATH, "resources", "client_secret.json"))
+
+            # Retry InstalledAppFlow creation
+            flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+        else:
+            logger.critical("Unhandled FileNotFound Exception in youtube_auth_oauth()", exc_info=exc_fnfe)
+            raise
     credentials = flow.run_console()
     logger.info("OAuth: Instantiated flow (console)")
     return build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
