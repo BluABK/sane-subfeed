4800a48032a7bde98c47a4f20734663c074b8ee1
BluABK <BluABK@users.noreply.github.com>
2018-12-13 16:03:48 +0100

    Improved upon the SaneExceptionHandler adding pop, add and clear functions as well as multiple choices on how to handle the exception.
    If app_ref parameter is not given, call the original excepthook, if not
    then either add it to a list or call a raise_exception() function in the
    app_ref "parent".

--- sane_yt_subfeed/gui/exception_handler/sane_exception_handler.py
+++ sane_yt_subfeed/gui/exception_handler/sane_exception_handler.py
@@ -1,3 +1,4 @@
+import copy
 import sys
 from PyQt5.QtCore import QObject, pyqtSignal
 
@@ -10,14 +11,26 @@ class SaneExceptionHandler(QObject):
     Instead it just exits with a status of 1 to show an exception was caught.
 
     This exception handler overrides the sys._excepthook in order to handle exceptions sanely.
+
+    Basic setup/usage example for a QApplication:
+    # Create the Exception Handler
+    exceptionHandler = SaneExceptionHandler()
+    # Back up the reference to the exceptionhook
+    sys._excepthook = sys.excepthook
+    # Set the exception hook to be wrapped by the Exception Handler
+    sys.excepthook = exceptionHandler.handler
     """
     exc_id = 0
+    exceptions = []
     errorSignal = pyqtSignal()
     silentSignal = pyqtSignal()
 
-    def __init__(self):
+    def __init__(self, app_ref=None, use_list=True):
         super(SaneExceptionHandler, self).__init__()
+        self.app_ref = app_ref
+        self.use_list = use_list
         self.logger = create_logger(__name__)
+
         # Back up the reference to the exception hook
         sys._excepthook = sys.excepthook
         self.original_excepthook = sys._excepthook
@@ -34,20 +47,56 @@ class SaneExceptionHandler(QObject):
         # Log the exception with the logger
         self.logger.exception("Intercepted Exception #{}".format(self.exc_id), exc_info=(exctype, value, traceback))
 
-        # Increment Exception Identifier
+        # Increment the exception identifier
         self.exc_id += 1
 
-        # Finally call the original Exception hook
-        self.original_excepthook(exctype, value, traceback)
+        # Finally call an exception hook
+        if self.app_ref is None:
+            # Call the original Exception hook
+            self.original_excepthook(exctype, value, traceback)
+        else:
+            if self.use_list:
+                # Add exception to a list to be handled later
+                self.add_exception(exctype, value, traceback)
+            else:
+                # Call custom raise_exception function in QApplication parent
+                self.app_ref.raise_exception(exctype, value, traceback)
 
+    def add_exception(self, exctype, value, traceback):
+        """
+        Adds a list of the constructors of an Exception to the exceptions list.
+        :param exctype:
+        :param value:
+        :param traceback:
+        :return:
+        """
+        self.exceptions.append([exctype, value, traceback])
 
-"""
-Basic setup/usage in a QApplication:
+    def clear_exceptions(self):
+        """
+        Clears the exceptions list.
+        :return:
+        """
+        self.exceptions.clear()
 
-# Create the Exception Handler
-exceptionHandler = SaneExceptionHandler()
-# Back up the reference to the exceptionhook
-sys._excepthook = sys.excepthook
-# Set the exception hook to be wrapped by the Exception Handler
-sys.excepthook = exceptionHandler.handler
-"""
+    def pop_exception(self):
+        """
+        Pops the last exception in the exceptions list.
+        :return:
+        """
+        try:
+            return self.exceptions.pop()
+        except IndexError:
+            pass
+
+    def pop_exceptions(self):
+        """
+        Pops the entire exceptions list.
+        :return:
+        """
+        try:
+            popped_exceptions = copy.copy(self.exceptions)
+            self.clear_exceptions()
+            return popped_exceptions
+        except IndexError:
+            pass
