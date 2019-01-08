55428d174edbdf6473c2d45cd1cf96804a00bd9c
BluABK <BluABK@users.noreply.github.com>
2019-01-08 14:59:40 +0100

    Added thumbnail overlay functionality to SubFeedView + some cleanup.

--- sane_yt_subfeed/gui/views/grid_view/playback/playback_grid_view_thumbnail_tile.py
+++ sane_yt_subfeed/gui/views/grid_view/playback/playback_grid_view_thumbnail_tile.py
@@ -8,7 +8,7 @@ from sane_yt_subfeed.gui.views.grid_view.thumbnail_tile import ThumbnailTile
 from sane_yt_subfeed.log_handler import create_logger
 
 OVERLAY_NO_FILE_PATH = os.path.join(ICONS_PATH, 'no_file.png')
-OVERLAY_DISMISSED_PATH = os.path.join(ICONS_PATH, 'dismissed.png')
+OVERLAY_DISCARDED_PATH = os.path.join(ICONS_PATH, 'dismissed.png')
 OVERLAY_WATCHED_PATH = os.path.join(ICONS_PATH, 'watched.png')
 THUMBNAIL_NA_PATH = os.path.join(RESOURCES_PATH, 'thumbnail_na.png')
 
@@ -20,18 +20,28 @@ class PlaybackGridViewThumbnailTile(ThumbnailTile):
         self.logger = create_logger(__name__)
 
     def add_overlay(self, painter, thumb):
-        url_as_path = read_config('Play', 'use_url_as_path')
-        show_watched = read_config('GridView', 'show_watched')
-        show_dismissed = read_config('GridView', 'show_dismissed')
+        """
+        Override inherited class to set custom overlay labels on thumbnail tiles.
 
-        if ((not self.parent.video.vid_path) and not url_as_path) or (self.parent.video.watched and show_watched) \
-                or (self.parent.video.discarded and show_dismissed):
+        Since only one overlay can be clearly displayed at a time it checks which to set
+        through a set of if cases ranking highest to lowest priority label.
+        :param painter:
+        :param thumb:
+        :return:
+        """
+        # Overlay conditions
+        url_as_path = read_config('Play', 'use_url_as_path')
+        watched = read_config('GridView', 'show_watched') and self.parent.video.watched
+        discarded = read_config('GridView', 'show_dismissed') and self.parent.video.discarded
+
+        if ((not self.parent.video.vid_path) and not url_as_path) or watched or discarded:
             if not self.parent.video.vid_path:
                 overlay = QPixmap(OVERLAY_NO_FILE_PATH)
             elif self.parent.video.watched:
                 overlay = QPixmap(OVERLAY_WATCHED_PATH)
             else:
-                overlay = QPixmap(OVERLAY_DISMISSED_PATH)
+                overlay = QPixmap(OVERLAY_DISCARDED_PATH)
+
             resize_ratio = min(thumb.width() * 0.7 / thumb.width(), thumb.height() * 0.3 / thumb.height())
             new_size = QSize(thumb.width() * resize_ratio, thumb.height() * resize_ratio)
             overlay = overlay.scaled(new_size, Qt.KeepAspectRatio, Qt.SmoothTransformation)
