5bb0b6c10b28114c21cb831e6da4af05e6c1e816
BluABK <BluABK@users.noreply.github.com>
2019-03-01 20:04:17 +0100

    Create a new httplib2.Http object per googleapiclient.discovery.build()
    The httplib2.Http() objects are not thread-safe so we need to send in
    our own separate http object instance for each build(). This should fix
    the long present "Too many files" stale file descriptors bug.

--- sane_yt_subfeed/authentication.py
+++ sane_yt_subfeed/authentication.py
@@ -1,3 +1,5 @@
+import apiclient
+import httplib2
 import json
 import os
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -24,18 +26,43 @@ def get_api_key():
     return keys['api_key']
 
 
+def build_request(http, *args, **kwargs):
+    """
+    The httplib2.Http() objects are not thread-safe. Create a new Http() object for every request and
+    override the construction of it within the service object.
+
+    https://developers.google.com/api-client-library/python/guide/thread_safety
+    :param http:
+    :param args:
+    :param kwargs:
+    :return:
+    """
+    new_http = httplib2.Http()
+    return apiclient.http.HttpRequest(new_http, *args, **kwargs)
+
+
 def youtube_auth_oauth():
     """
-    Authorize the request and store authorization credentials.
+    Authorize the request using OAuth and store authorization credentials.
+
+    OAuth is required for most higher level user actions like accessing user's subscriptions.
     :return:
     """
     logger.info("OAuth: Authorising API...")
     flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
     credentials = flow.run_console()
     logger.info("OAuth: Instantiated flow (console)")
+    # Note: If you try to send in requestBuilder here it will fail, but OAuth isn't threaded so it should be fine...
     return build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
 
 
 def youtube_auth_keys():
+    """
+    Authorize the request using an API Key and store authorization credentials.
+
+    API Keys are easier to use than needing to OAuth for every single resource.
+    :return:
+    """
     logger.info("Keys: Authorising API...")
-    return build(API_SERVICE_NAME, API_VERSION, developerKey=get_api_key())
+    # Send in requestBuilder to override the construction of non-thread safe http obj within the service object.
+    return build(API_SERVICE_NAME, API_VERSION, developerKey=get_api_key(), requestBuilder=build_request)
