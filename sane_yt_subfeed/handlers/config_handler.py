ef65c08cb33e64097eaf870d3dbb24dc5cc92512
BluABK <BluABK@users.noreply.github.com>
2019-04-06 14:09:41 +0200

    Fixed config_handler root path pointing at wrong level after move.

--- sane_yt_subfeed/handlers/config_handler.py
+++ sane_yt_subfeed/handlers/config_handler.py
@@ -4,11 +4,12 @@ import os
 from configparser import ConfigParser, NoSectionError, NoOptionError
 from shutil import copyfile
 
-OS_PATH = os.path.dirname(__file__)
-CONFIG_PATH = os.path.join(OS_PATH, '..', 'config.ini')
-CONFIG_HOTKEYS_PATH = os.path.join(OS_PATH, '..', 'hotkeys.ini')
-SAMPLE_HOTKEYS_PATH = os.path.join(OS_PATH, '..', 'hotkeys.ini.sample')
-SAMPLE_PATH = os.path.join(OS_PATH, '..', 'config.ini.sample')
+from sane_yt_subfeed.absolute_paths import ROOT_PATH
+
+CONFIG_PATH = os.path.join(ROOT_PATH, '..', 'config.ini')
+CONFIG_HOTKEYS_PATH = os.path.join(ROOT_PATH, '..', 'hotkeys.ini')
+SAMPLE_HOTKEYS_PATH = os.path.join(ROOT_PATH, '..', 'hotkeys.ini.sample')
+SAMPLE_PATH = os.path.join(ROOT_PATH, '..', 'config.ini.sample')
 
 parser = None
 default_parser = ConfigParser()
@@ -168,7 +169,7 @@ DEFAULTS = {
         'logging_port': '19996'
     },
     'Database': {
-        'url': 'sqlite:///{}'.format(os.path.join(OS_PATH, 'resources', 'permanents.db'))
+        'url': 'sqlite:///{}'.format(os.path.join(ROOT_PATH, 'resources', 'permanents.db'))
     }
 }
 
