BluABK/sane-subfeed

commit 2da05b28a387216c59e1b555bb59fd776736aa12

Moved config_handler into handlers package.

sane_yt_subfeed/database/orm.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from sane_yt_subfeed.config_handler import read_config
+from sane_yt_subfeed.handlers.config_handler import read_config
 from sane_yt_subfeed.log_handler import create_logger
 
 # FIXME: module level logger not suggested: https://fangpenlin.com/posts/2012/08/26/good-logging-practice-in-python/
@@ -26,9 +26,6 @@ def init_db():
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
-    import sane_yt_subfeed.database.models
-    import sane_yt_subfeed.database.video
-    import sane_yt_subfeed.database.db_download_tile
     PermanentBase.metadata.create_all(bind=engine)
     logger.info("Initialised DB (PermanentBase.metadata.create_all(bind=engine))")
 

sane_yt_subfeed/cli.py
@@ -4,7 +4,7 @@ import click
 import datetime
 from sqlalchemy import or_, and_
 
-from sane_yt_subfeed.config_handler import read_config
+from sane_yt_subfeed.handlers.config_handler import read_config
 from sane_yt_subfeed.database.orm import db_session
 from sane_yt_subfeed.database.video import Video
 from sane_yt_subfeed.log_handler import create_logger

sane_yt_subfeed/controller/controller.py
@@ -5,7 +5,7 @@ import sys
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication
 
-from sane_yt_subfeed.config_handler import read_config
+from sane_yt_subfeed.handlers.config_handler import read_config
 from sane_yt_subfeed.controller.view_models import MainModel
 from sane_yt_subfeed.gui.main_window.main_window import MainWindow
 from sane_yt_subfeed.log_handler import create_logger

sane_yt_subfeed/controller/listeners/gui/main_window/main_window_listener.py
@@ -4,7 +4,6 @@ import time
 from PyQt5.QtCore import *
 
 from sane_yt_subfeed import main
-from sane_yt_subfeed.config_handler import read_config
 from sane_yt_subfeed.controller.listeners.gui.views.download_view.download_view_listener import DownloadViewListener
 from sane_yt_subfeed.log_handler import create_logger
 from sane_yt_subfeed.youtube.thumbnail_handler import download_thumbnails_threaded

sane_yt_subfeed/controller/listeners/gui/views/download_view/download_view_listener.py
@@ -6,7 +6,7 @@ from PyQt5.QtCore import QObject, pyqtSignal
 from sqlalchemy import false
 
 from sane_yt_subfeed import create_logger
-from sane_yt_subfeed.config_handler import read_config
+from sane_yt_subfeed.handlers.config_handler import read_config
 import sane_yt_subfeed.controller.listeners.gui.views.grid_view.static_grid_view_listener as static_grid_view_listener
 from sane_yt_subfeed.database.db_download_tile import DBDownloadTile
 from sane_yt_subfeed.database.detached_models.d_db_download_tile import DDBDownloadTile

sane_yt_subfeed/controller/listeners/gui/views/grid_view/grid_view_listener.py
@@ -1,8 +1,6 @@
-import time
+from PyQt5.QtCore import QObject, pyqtSlot
 
-from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot
-
-from sane_yt_subfeed.config_handler import read_config
+from sane_yt_subfeed.handlers.config_handler import read_config
 # from sane_yt_subfeed.controller.listeners.gui.views.download_view.download_view_listener import DownloadViewListener
 from sane_yt_subfeed.database.video import Video
 from sane_yt_subfeed.database.write_operations import UpdateVideo

sane_yt_subfeed/controller/listeners/gui/views/grid_view/playback/playback_grid_view_listener.py
@@ -1,7 +1,7 @@
 from PyQt5.QtCore import pyqtSignal, pyqtSlot
 
 from sane_yt_subfeed import create_logger
-from sane_yt_subfeed.config_handler import read_config
+from sane_yt_subfeed.handlers.config_handler import read_config
 from sane_yt_subfeed.controller.listeners.gui.views.grid_view.grid_view_listener import GridViewListener
 from sane_yt_subfeed.controller.listeners.gui.views.download_view.download_view_listener import DownloadViewListener
 from sane_yt_subfeed.database.detached_models.video_d import VideoD

sane_yt_subfeed/controller/listeners/youtube_dir_listener/youtube_dir_listener.py
@@ -1,4 +1,3 @@
-import gc
 import time
 
 import datetime
@@ -6,7 +5,7 @@ import os
 from PyQt5.QtCore import *
 from watchdog.observers import Observer
 
-from sane_yt_subfeed.config_handler import read_config
+from sane_yt_subfeed.handlers.config_handler import read_config
 from sane_yt_subfeed.controller.dir_handler import VidEventHandler, CheckYoutubeFolderForNew
 from sane_yt_subfeed.database.orm import db_session
 from sane_yt_subfeed.database.video import Video

commit 09c17e6dc0b6bdb61e895a2a1b7fd895e148cf3c

Fixed critical DB creation failure bug introduced in commit 2da05b2.
IDE refactor move decided to remove "unused" imports >_<

sane_yt_subfeed/database/orm.py
@@ -26,6 +26,9 @@ def init_db():
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
+    import sane_yt_subfeed.database.models
+    import sane_yt_subfeed.database.video
+    import sane_yt_subfeed.database.db_download_tile
     PermanentBase.metadata.create_all(bind=engine)
     logger.info("Initialised DB (PermanentBase.metadata.create_all(bind=engine))")
 